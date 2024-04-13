364a1eeddccdb7031c11f1c489e35c1889dec645
Matija Osrečki <github@matijao.com>
2024-04-14 00:43:45 +0200

    fix: reword

--- pages/about.vue
+++ pages/about.vue
@@ -12,10 +12,10 @@
       <div class="max-w-xl">
         <div class="text-default-foreground mt-4 max-w-xl leading-7 prose">
           <p>
-            I'm a determined Software Engineer from Croatia with a strong sense of good UX and design, great attention to detail and a relentless focus on quality. Proficient in Vue, Nuxt, and full-stack TypeScript, I thrive on transforming ideas into polished products built from scratch. My deep curiosity and strong passion for UX/UI design, awesome developer experience, clean code and open-source fuel my work.
+            I'm a determined Software Engineer from Croatia with a strong sense of good UX and design, meticulous attention to detail and a relentless focus on quality. Proficient in Vue, Nuxt, and full-stack TypeScript, I thrive on transforming ideas into polished products built from scratch. My deep curiosity and strong passion for UX/UI design, awesome developer experience, clean code and open-source fuel my work.
           </p>
 
-          <p>Beyond code, I'm deeply passionate about Bitcoin, dedicating my time to advocating for preserving privacy, fostering self-sovereignty, enhancing financial literacy, and cultivating a low time preference mindset.</p>
+          <p>Beyond code, I'm deeply passionate about Bitcoin, dedicating my time to advocating for preserving our privacy, fostering self-sovereignty, enhancing financial literacy, and cultivating a low time preference mindset.</p>
         </div>
       </div>
     </div>
