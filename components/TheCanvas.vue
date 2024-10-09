482cf39534baf6f773f37e35cea71d1cd249baa0
Matija Osrečki <github@matijao.com>
2024-10-09 18:52:14 +0200

    feat: dirty hotfix for canvas init mousemove issue

--- components/TheCanvas.vue
+++ components/TheCanvas.vue
@@ -43,21 +43,93 @@ const config = {
   RUN_INITIALLY: false,
 }
 
+function getWebGLContext(canvas) {
+  const params = { alpha: true, depth: false, stencil: false, antialias: false, preserveDrawingBuffer: false }
+
+  let gl = canvas.getContext('webgl2', params)
+  const isWebGL2 = !!gl
+  if (!isWebGL2) { gl = canvas.getContext('webgl', params) || canvas.getContext('experimental-webgl', params) }
+
+  let halfFloat
+  let supportLinearFiltering
+  if (isWebGL2) {
+    gl.getExtension('EXT_color_buffer_float')
+    supportLinearFiltering = gl.getExtension('OES_texture_float_linear')
+  } else {
+    halfFloat = gl.getExtension('OES_texture_half_float')
+    supportLinearFiltering = gl.getExtension('OES_texture_half_float_linear')
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+  const halfFloatTexType = isWebGL2 ? gl.HALF_FLOAT : halfFloat.HALF_FLOAT_OES
+  let formatRGBA
+  let formatRG
+  let formatR
+
+  if (isWebGL2) {
+    formatRGBA = getSupportedFormat(gl, gl.RGBA16F, gl.RGBA, halfFloatTexType)
+    formatRG = getSupportedFormat(gl, gl.RG16F, gl.RG, halfFloatTexType)
+    formatR = getSupportedFormat(gl, gl.R16F, gl.RED, halfFloatTexType)
+  } else {
+    formatRGBA = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType)
+    formatRG = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType)
+    formatR = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType)
+  }
+
+  return {
+    gl,
+    ext: {
+      formatRGBA,
+      formatRG,
+      formatR,
+      halfFloatTexType,
+      supportLinearFiltering,
+    },
+  }
+}
+
+function getSupportedFormat(gl, internalFormat, format, type) {
+  if (!supportRenderTextureFormat(gl, internalFormat, format, type)) {
+    switch (internalFormat) {
+      case gl.R16F:
+        return getSupportedFormat(gl, gl.RG16F, gl.RG, type)
+      case gl.RG16F:
+        return getSupportedFormat(gl, gl.RGBA16F, gl.RGBA, type)
+      default:
+        return null
+    }
+  }
+
+  return {
+    internalFormat,
+    format,
+  }
+}
+
+function supportRenderTextureFormat(gl, internalFormat, format, type) {
+  const texture = gl.createTexture()
+  gl.bindTexture(gl.TEXTURE_2D, texture)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+  gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, 4, 4, 0, format, type, null)
+
+  const fbo = gl.createFramebuffer()
+  gl.bindFramebuffer(gl.FRAMEBUFFER, fbo)
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
+
+  const status = gl.checkFramebufferStatus(gl.FRAMEBUFFER)
+  return status == gl.FRAMEBUFFER_COMPLETE
+}
+
+function isMobile() {
+  return /Mobi|Android/i.test(navigator.userAgent)
+}
+
 const initCanvas = (canvas) => {
   resizeCanvas()
-
-  function PointerPrototype() {
-    this.id = -1
-    this.texcoordX = 0
-    this.texcoordY = 0
-    this.prevTexcoordX = 0
-    this.prevTexcoordY = 0
-    this.deltaX = 0
-    this.deltaY = 0
-    this.down = false
-    this.moved = false
-    this.color = [30, 0, 300]
-  }
 
   const pointers = []
   const splatStack = []
@@ -75,89 +147,17 @@ const initCanvas = (canvas) => {
     config.SUNRAYS = false
   }
 
-  function getWebGLContext(canvas) {
-    const params = { alpha: true, depth: false, stencil: false, antialias: false, preserveDrawingBuffer: false }
-
-    let gl = canvas.getContext('webgl2', params)
-    const isWebGL2 = !!gl
-    if (!isWebGL2) { gl = canvas.getContext('webgl', params) || canvas.getContext('experimental-webgl', params) }
-
-    let halfFloat
-    let supportLinearFiltering
-    if (isWebGL2) {
-      gl.getExtension('EXT_color_buffer_float')
-      supportLinearFiltering = gl.getExtension('OES_texture_float_linear')
-    } else {
-      halfFloat = gl.getExtension('OES_texture_half_float')
-      supportLinearFiltering = gl.getExtension('OES_texture_half_float_linear')
-    }
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0)
-
-    const halfFloatTexType = isWebGL2 ? gl.HALF_FLOAT : halfFloat.HALF_FLOAT_OES
-    let formatRGBA
-    let formatRG
-    let formatR
-
-    if (isWebGL2) {
-      formatRGBA = getSupportedFormat(gl, gl.RGBA16F, gl.RGBA, halfFloatTexType)
-      formatRG = getSupportedFormat(gl, gl.RG16F, gl.RG, halfFloatTexType)
-      formatR = getSupportedFormat(gl, gl.R16F, gl.RED, halfFloatTexType)
-    } else {
-      formatRGBA = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType)
-      formatRG = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType)
-      formatR = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType)
-    }
-
-    return {
-      gl,
-      ext: {
-        formatRGBA,
-        formatRG,
-        formatR,
-        halfFloatTexType,
-        supportLinearFiltering,
-      },
-    }
-  }
-
-  function getSupportedFormat(gl, internalFormat, format, type) {
-    if (!supportRenderTextureFormat(gl, internalFormat, format, type)) {
-      switch (internalFormat) {
-        case gl.R16F:
-          return getSupportedFormat(gl, gl.RG16F, gl.RG, type)
-        case gl.RG16F:
-          return getSupportedFormat(gl, gl.RGBA16F, gl.RGBA, type)
-        default:
-          return null
-      }
-    }
-
-    return {
-      internalFormat,
-      format,
-    }
-  }
-
-  function supportRenderTextureFormat(gl, internalFormat, format, type) {
-    const texture = gl.createTexture()
-    gl.bindTexture(gl.TEXTURE_2D, texture)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-    gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, 4, 4, 0, format, type, null)
-
-    const fbo = gl.createFramebuffer()
-    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo)
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
-
-    const status = gl.checkFramebufferStatus(gl.FRAMEBUFFER)
-    return status == gl.FRAMEBUFFER_COMPLETE
-  }
-
-  function isMobile() {
-    return /Mobi|Android/i.test(navigator.userAgent)
+  function PointerPrototype() {
+    this.id = -1
+    this.texcoordX = 0
+    this.texcoordY = 0
+    this.prevTexcoordX = 0
+    this.prevTexcoordY = 0
+    this.deltaX = 0
+    this.deltaY = 0
+    this.down = false
+    this.moved = false
+    this.color = [30, 0, 300]
   }
 
   class Material {
@@ -1216,34 +1216,37 @@ const initCanvas = (canvas) => {
     return radius
   }
 
-  document.addEventListener('mousemove', (e) => {
-    const [pointer] = pointers
+  // TODO: dirty hack for now
+  setTimeout(() => {
+    document.addEventListener('mousemove', (e) => {
+      const [pointer] = pointers
 
-    // TODO: separate this from main config
-    if (config.PAUSED) {
-      return
-    }
+      // TODO: separate this from main config
+      if (config.PAUSED) {
+        return
+      }
 
-    // Get the bounding rectangle of the canvas
-    const rect = canvas.getBoundingClientRect()
+      // Get the bounding rectangle of the canvas
+      const rect = canvas.getBoundingClientRect()
 
-    // Calculate the mouse position relative to the canvas
-    const posX = scaleByPixelRatio(e.clientX - rect.left)
-    const posY = scaleByPixelRatio(e.clientY - rect.top)
+      // Calculate the mouse position relative to the canvas
+      const posX = scaleByPixelRatio(e.clientX - rect.left)
+      const posY = scaleByPixelRatio(e.clientY - rect.top)
 
-    updatePointerMoveData(pointer, posX, posY)
-  })
+      updatePointerMoveData(pointer, posX, posY)
+    })
 
-  canvas.addEventListener('touchstart', (e) => {
-    e.preventDefault()
-    const touches = e.targetTouches
-    while (touches.length >= pointers.length) { pointers.push(new PointerPrototype()) }
-    for (let i = 0; i < touches.length; i++) {
-      const posX = scaleByPixelRatio(touches[i].pageX)
-      const posY = scaleByPixelRatio(touches[i].pageY)
-      updatePointerDownData(pointers[i + 1], touches[i].identifier, posX, posY)
-    }
-  })
+    canvas.addEventListener('touchstart', (e) => {
+      e.preventDefault()
+      const touches = e.targetTouches
+      while (touches.length >= pointers.length) { pointers.push(new PointerPrototype()) }
+      for (let i = 0; i < touches.length; i++) {
+        const posX = scaleByPixelRatio(touches[i].pageX)
+        const posY = scaleByPixelRatio(touches[i].pageY)
+        updatePointerDownData(pointers[i + 1], touches[i].identifier, posX, posY)
+      }
+    })
+  }, 200)
 
   window.addEventListener('keydown', (e) => {
     // if (e.code === 'KeyP') { config.PAUSED = !config.PAUSED }
@@ -1371,6 +1374,15 @@ onMounted(() => {
   if (canvas) {
     console.log('Init canvas')
     initCanvas(canvas)
+
+    canvas.addEventListener('mousemove', (e) => {
+      if (!initialized.value) { return } // Ensure the canvas is ready
+      const [pointer] = pointers
+      const rect = canvas.getBoundingClientRect()
+      const posX = scaleByPixelRatio(e.clientX - rect.left)
+      const posY = scaleByPixelRatio(e.clientY - rect.top)
+      updatePointerMoveData(pointer, posX, posY)
+    })
   } else {
     console.warn('Canvas not found')
   }
